9972139fca94b98687773bf884b120d202c17bff
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-05 23:54:37 -0700

    add startup configurations

---
+++ HairSalon/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using HairSalon.Models;
+
+namespace HairSalon
+{
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
+    {
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile("appsettings.json");
+      Configuration = builder.Build();
+    }
+
+    public IConfigurationRoot Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<HairSalonContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseStaticFiles();
+
+      app.UseDeveloperExceptionPage();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Something went wrong!");
+      });
+    }
+    public static class DBConfiguration
+    {
+      public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=jose_amesquita;";
+    }
+  }
+}
