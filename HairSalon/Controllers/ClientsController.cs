d3c205f3057749e2c75ed97f1913e60affac8470
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-06 17:11:29 -0700

    Name is replaced with StylistName

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -24,7 +24,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
       return View();
     }
 
