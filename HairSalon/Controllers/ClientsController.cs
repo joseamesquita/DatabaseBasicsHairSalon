cb43cead19cf2a8e74b8aaf35ce1c95b40e233d6
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-06 17:14:32 -0700

    load the Stylist objects for each client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -18,7 +18,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
       return View(model);
     }
 
